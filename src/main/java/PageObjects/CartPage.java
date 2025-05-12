ce902927a72bd495623e86560c21d2d4ee2c4a88
Charithaganga31 <charithagangamanukonda@gmail.com>
2025-05-12 22:04:10 +0530

    Update CartPage.java

--- src/main/java/PageObjects/CartPage.java
+++ src/main/java/PageObjects/CartPage.java
@@ -7,8 +7,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-import org.testng.Assert;
-
 import CommonActions.Abstract;
 
 public class CartPage extends Abstract{
